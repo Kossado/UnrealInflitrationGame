f2ea715bb80baeff36562f07278379cddb2654c4
yannCHANTREL <74556830+yannCHANTREL@users.noreply.github.com>
2021-10-19 15:50:08 +0200

    little modification

--- Source/GC_UE4CPP/HUD/GC_PauseMenuWidget.cpp
+++ Source/GC_UE4CPP/HUD/GC_PauseMenuWidget.cpp
@@ -1,8 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GC_PauseMenuWidget.h"
-
 #include "MainGameMode.h"
 #include "Blueprint/WidgetBlueprintLibrary.h"
 
