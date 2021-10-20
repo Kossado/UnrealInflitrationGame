ea7820165d3f06ee3aa99541f2664b3387d0ee09
yannCHANTREL <74556830+yannCHANTREL@users.noreply.github.com>
2021-10-20 15:54:52 +0200

    Create torch via point light, and gestion light map

---
+++ Source/GC_UE4CPP/Private/Torch.cpp
@@ -0,0 +1,31 @@
+#include "Torch.h"
+
+ATorch::ATorch()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Set up Scene Component
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(FName(TEXT("Scene Component")));
+	RootComponent = SceneComponent;
+
+	//Set up static mesh
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName(TEXT("Static Mesh")));
+	StaticMesh->SetupAttachment(RootComponent);
+
+	//Set up light
+	LightMesh = CreateDefaultSubobject<UPointLightComponent>(FName(TEXT("Light Mesh")));
+	LightMesh->SetupAttachment(RootComponent);
+}
+
+void ATorch::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATorch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/GC_UE4CPP/Public/Torch.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PointLightComponent.h"
+#include "GameFramework/Actor.h"
+#include "Torch.generated.h"
+
+UCLASS()
+class GC_UE4CPP_API ATorch : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATorch();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category=Mesh, meta = (AllowPrivateAccess = "true"))
+	USceneComponent* SceneComponent;
+
+	UPROPERTY(VisibleAnywhere, Category=Mesh, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(VisibleAnywhere, Category=Mesh, meta = (AllowPrivateAccess = "true"))
+	UPointLightComponent* LightMesh;
+
+};
